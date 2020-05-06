630943b4273aedb09a54acc8a1623b702cd32c4d
Ilya Tretyakov <64084424+iltret@users.noreply.github.com>
2020-05-06 17:27:12 +0500

    Add files via upload

---
+++ 6/6.1/6.1/6.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n;
+	n = rand() % 40 + 10;
+	cout << n << endl;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 100 - 50;
+		cout << a[i] << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] % 2 == 0)
+		{
+			cout << a[i] << ' ';
+			a[i] = a[i + 1];
+		}
+	}
+	delete[] a;
+	return 0;
+}
---
+++ 6/6.2/66/66.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+
+    srand(time(NULL));
+
+    int s = 0, d = 0;
+
+
+    cout << "Введите количество строк" << "\n";
+    cin >> s;
+    cout << "Введите количество столбцов" << "\n";
+    cin >> d;
+
+    int** Array = new int* [s];
+    for (int i = 0; i < s; i++)
+        Array[i] = new int[d];
+
+
+    for (int i = 0; i < s; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            Array[i][j] = rand() % 100;
+            cout << Array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+
+    int k = 0;
+    cout << "Введите количество строк для добавления: ";
+    cin >> k;
+    int** Barray = new int* [s + k];
+    for (int i = 0; i < s + k; i++)
+        Barray[i] = new int[d];
+
+    for (int i = 0; i < s + k; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            if (i >= k)
+                Barray[i][j] = Array[i - k][j];
+            else
+                Barray[i][j] = rand() % 100;
+            cout << Barray[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+   
+
+}
+
