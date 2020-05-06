ad42c03c9f9ca2121c124553c38472038f43da6e
Ilya Tretyakov <64084424+iltret@users.noreply.github.com>
2020-05-06 17:51:41 +0500

    Add files via upload

---
+++ 12/12/12.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+using namespace std;
+int board[8][8];
+void setFerz(int i, int j)
+{
+    for (int x = 0; x < 8; x++)
+    {
+        board[x][j]++;
+        board[i][x]++;
+        int foo;
+        foo = j - i + x;
+        if (foo >= 0 && foo < 8)
+        {
+            board[x][foo]++;
+        }
+        foo = j + i - x;
+        if (foo >= 0 && foo < 8)
+        {
+            board[x][foo]++;
+        }
+    }
+    board[i][j] = 1;
+}
+void resetFerz(int i, int j)
+{
+    for (int x = 0; x < 8; x++)
+    {
+        board[x][j]--;
+        board[i][x]--;
+        int foo;
+        foo = j - i + x;
+        if (foo >= 0 && foo < 8)
+        {
+            board[x][foo]--;
+        }
+        foo = j + i - x;
+        if (foo >= 0 && foo < 8)
+        {
+            board[x][foo]--;
+        }
+    }
+    board[i][j] = 0;
+}
+bool tryFerz(int i)
+{
+    bool result = false;
+    for (int j = 0; j < 8; j++)
+    {
+        if (board[i][j] == 0)
+        {
+            setFerz(i, j);
+            if (i == 7)
+            {
+                result = true;
+            }
+            else if (!(result = tryFerz(i + 1)))
+            {
+                resetFerz(i, j);
+            }
+        }
+        if (result)
+            break;
+    }
+    return result;
+}
+int main()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
+    tryFerz(0);
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j] == 1)
+            {
+                cout << "[]";
+            }
+            else
+            {
+                cout << " .";
+            }
+        }
+        cout << endl;
+    }
+}
